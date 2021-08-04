abd557ddd9f46c714ffc3a98f7dac60f0456985f
Patraporn Leelaprachakul <p.lee@outlook.com>
2021-08-04 10:59:07 -0400

    init

---
+++ src/webparts/staffing/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IStaffingWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'StaffingWebPartStrings' {
+  const strings: IStaffingWebPartStrings;
+  export = strings;
+}
